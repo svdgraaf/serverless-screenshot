624391fd23677ab7c9a7533c595c0e29ebf423b3
Sander van de Graaf <mail@svdgraaf.nl>
2016-09-21 09:57:23 +0200

    added checker for valid url

--- handler.js
+++ handler.js
@@ -6,30 +6,37 @@ var crypto = require('crypto');
 var fs = require('fs');
 var url = require('url');
 var AWS = require('aws-sdk');
+var validUrl = require('valid-url');
 
-console.log(AWS.VERSION)
 
 // screenshot the given url
 module.exports.screenshot = (event, context, cb) => {
   const target_url = event['query']['url']
+
+  // check if the given url is valid
+  if(!validUrl.isUri(target_url)) {
+    cb(`422, please provide a valid url, not: ${target_url}`);
+    return false;
+  }
+
   const target_hostname = url.parse(target_url).hostname;
   const target_bucket = process.env.BUCKET_NAME;
-  console.log(process.env)
   const target_hash = crypto.createHash('md5').update(target_url).digest("hex");
   const target_filename = `${target_hash}/${target_hostname}/${target_hash}.png`
   console.log(`Snapshotting ${target_url} to s3://${target_bucket}/${target_filename}`);
 
   // build the cmd for phantom to render the url
-  // const cmd =`./phantomjs/phantomjs_linux-x86_64 --debug=yes --ignore-ssl-errors=true ./phantomjs/screenshot.js ${target_url} /tmp/${target_hash}.png 1280 1024`;
-  const cmd =`./phantomjs/phantomjs_osx --debug=yes --ignore-ssl-errors=true ./phantomjs/screenshot.js ${target_url} /tmp/${target_hash}.png 1280 1024`;
+  const cmd =`./phantomjs/phantomjs_linux-x86_64 --debug=yes --ignore-ssl-errors=true ./phantomjs/screenshot.js ${target_url} /tmp/${target_hash}.png 1280 1024`;
+  // const cmd =`./phantomjs/phantomjs_osx --debug=yes --ignore-ssl-errors=true ./phantomjs/screenshot.js ${target_url} /tmp/${target_hash}.png 1280 1024`;
   console.log(cmd);
 
   // run the phantomjs command
   exec(cmd, function (error, stdout, stderr) {
     if (error !== null) {
       // the command failed (non-zero), fail
-      cb('500, please try again' + error);
+      cb('422, please try again' + error);
       console.log('exec error: ' + error, stdout, stderr);
+      return false;
     } else {
       // snapshotting succeeded, let's upload to S3
 
