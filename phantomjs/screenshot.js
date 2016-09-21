c26ba29db94710a0839228efa02e2863ad1bae29
Sander van de Graaf <mail@svdgraaf.nl>
2016-09-21 09:40:54 +0200

    added phantomjs binaries

---
+++ phantomjs/screenshot.js
@@ -0,0 +1,41 @@
+var page = require('webpage').create(),
+    system = require('system'),
+    url, output, width, height;
+
+page.onResourceReceived = function(response) {
+    // console.log(JSON.stringify(response, undefined, 4));
+    if(response.id == 1 && response.status != 200){
+        console.log('Received a non-200 OK response, got: ', response.status);
+        phantom.exit(1);
+    }
+}
+
+address = system.args[1];
+output = system.args[2];
+width = system.args[3];
+height = system.args[4];
+
+timeout = 1000;
+console.log("Args: ", system.args);
+console.log("Screenshotting: ", address, ", to: ", output);
+
+page.viewportSize = { width: parseInt(width), height: parseInt(height) };
+console.log("Viewport: ", JSON.stringify(page.viewportSize));
+
+page.open(address, function (status) {
+    if (status !== 'success') {
+        console.log('Unable to load the address!');
+        phantom.exit();
+    } else {
+        window.setTimeout(function () {
+            page.render(output);
+            phantom.exit();
+        }, timeout);
+    }
+});
+//
+// var page = require('webpage').create();
+// page.open('http://github.com/', function() {
+//   page.render('github.png');
+//   phantom.exit();
+// });
