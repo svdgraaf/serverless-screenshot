e814f3be4161a3733f19eaf6177b7974cd8b6f68
Sander van de Graaf <mail@svdgraaf.nl>
2016-09-28 13:03:19 +0200

    force region

--- test.js
+++ test.js
@@ -2,11 +2,17 @@ var path = require('path');
 var fs = require('fs');
 
 var handler = require( path.resolve( __dirname, "./handler.js" ) );
-var input = JSON.parse(fs.readFileSync('event.json', 'utf8'));
+// var input = JSON.parse(fs.readFileSync('events/put_object.json', 'utf8'));
+var input = JSON.parse(fs.readFileSync('events/create.json', 'utf8'));
 
-var callback = function(data) {
-  console.log('got data: '+data);
+var callback = function(err, data ) {
+  if(err) {
+    console.warn(data);
+  } else {
+    console.log(data);
+  }
 };
 
-// handler.take_screenshot(input, null, callback);
-handler.list_screenshots(input, null, callback);
+// handler.create_thumbnails(input, null, callback);
+handler.take_screenshot(input, null, callback);
+// handler.list_screenshots(input, null, callback);
